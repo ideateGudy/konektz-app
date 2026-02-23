7cf654da8277ec8c65397f26e28362ae88b8b0c8
Goodnews Azonubi <azonubigoodnews@gmail.com>
2026-02-23 18:21:22 +0100

    feat(auth/presentation/widgets): create AuthInputField widget for user input

---
+++ lib/features/auth/presentaion/widgets/auth_input_field.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:konektz/core/theme.dart';
+
+class AuthInputField extends StatelessWidget {
+  final String hint;
+  final IconData icon;
+  final TextEditingController controller;
+  final bool obscureText;
+
+  const AuthInputField({
+    super.key,
+    required this.hint,
+    required this.icon,
+    required this.controller,
+    this.obscureText = false,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: DefaultColors.sendMessageInput,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          Icon(icon, color: Colors.grey),
+          const SizedBox(width: 10),
+          Expanded(
+            child: TextField(
+              controller: controller,
+              obscureText: obscureText,
+              decoration: InputDecoration(
+                hintText: hint,
+                hintStyle: const TextStyle(color: Colors.grey),
+                border: InputBorder.none,
+              ),
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
