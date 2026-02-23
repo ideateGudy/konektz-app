078cd74aab153a3757be6367939e2e9002ecad9f
Goodnews Azonubi <azonubigoodnews@gmail.com>
2026-02-23 18:19:25 +0100

    feat(auth/domain/useCases): implement RegisterUseCase for user registration logic

---
+++ lib/features/auth/domain/usecases/register_use_case.dart
@@ -0,0 +1,12 @@
+import 'package:konektz/features/auth/domain/entities/user_entity.dart';
+import 'package:konektz/features/auth/domain/repositories/auth_repository.dart';
+
+class RegisterUseCase {
+  final AuthRepository repository;
+
+  RegisterUseCase({required this.repository});
+
+  Future<UserEntity> call(String username, String email, String password) {
+    return repository.register(username, email, password);
+  }
+}
