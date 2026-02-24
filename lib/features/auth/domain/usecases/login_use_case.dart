eb64dca6f52d103e98a8666b9ab67709c315facf
Goodnews Azonubi <azonubigoodnews@gmail.com>
2026-02-24 14:42:07 +0100

    feat(auth/domain/useCases/login_use_case): integrate AuthValidator for login field validation

--- lib/features/auth/domain/usecases/login_use_case.dart
+++ lib/features/auth/domain/usecases/login_use_case.dart
@@ -1,12 +1,18 @@
 import 'package:konektz/features/auth/domain/entities/user_entity.dart';
 import 'package:konektz/features/auth/domain/repositories/auth_repository.dart';
+import 'package:konektz/features/auth/domain/validators/auth_validator.dart';
 
 class LoginUseCase {
   final AuthRepository repository;
+  final AuthValidator validator;
 
-  LoginUseCase({required this.repository});
+  LoginUseCase({
+    required this.repository,
+    this.validator = const AuthValidator(),
+  });
 
   Future<UserEntity> call(String email, String password) {
+    validator.validateLoginFields(email: email, password: password);
     return repository.login(email, password);
   }
 }
