4cc466213bd54526e9a2459a4722d8eb41c5ccf7
Goodnews Azonubi <azonubigoodnews@gmail.com>
2026-02-24 14:41:57 +0100

    feat(auth/domain/useCases/register_usecase): add AuthValidator to RegisterUseCase for field validation

--- lib/features/auth/domain/usecases/register_use_case.dart
+++ lib/features/auth/domain/usecases/register_use_case.dart
@@ -1,12 +1,22 @@
 import 'package:konektz/features/auth/domain/entities/user_entity.dart';
 import 'package:konektz/features/auth/domain/repositories/auth_repository.dart';
+import 'package:konektz/features/auth/domain/validators/auth_validator.dart';
 
 class RegisterUseCase {
   final AuthRepository repository;
+  final AuthValidator validator;
 
-  RegisterUseCase({required this.repository});
+  RegisterUseCase({
+    required this.repository,
+    this.validator = const AuthValidator(),
+  });
 
   Future<UserEntity> call(String username, String email, String password) {
+    validator.validateRegisterFields(
+      username: username,
+      email: email,
+      password: password,
+    );
     return repository.register(username, email, password);
   }
 }
